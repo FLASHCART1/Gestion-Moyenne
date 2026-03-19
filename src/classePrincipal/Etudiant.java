45ee0d64c3f6888836bea8df7402f6490877220b
FLASHCART1 <juliusmarius01@gmail.com>
2026-03-19 14:41:35 +0000

    Update Etudiant.java

--- src/classePrincipal/Etudiant.java
+++ src/classePrincipal/Etudiant.java
@@ -1,7 +1,15 @@
 package classePrincipal;
 
+import java.util.ArrayList;
+
 public class Etudiant {
 	private String nom;
 	private String prenom;
-	private Gr_note control;
+	private ArrayList<Evaluation> list;
+	
+	public void ajouter_note(double n, double c, int b) {
+		Evaluation a = new Evaluation(n, c, b);
+		list.add(a);
+		
+	}
 }
