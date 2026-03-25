7ec3b9fcb74989ed0ca59afea9a4a8fa0643f53a
FLASHCART1 <juliusmarius01@gmail.com>
2026-03-25 12:56:35 +0000

    update main.java

--- src/interFace/Main.java
+++ src/interFace/Main.java
@@ -1,16 +1,29 @@
 package interFace;
 
 import classePrincipal.*;
+import java.util.Scanner;
 
 public class Main {
+	public static void mainMenu() {
+		System.out.println("╔══════════════════════════════════╗");
+		System.out.println("║   GESTION DE MOYENNE EN JAVA     ║");
+		System.out.println("╚══════════════════════════════════╝");
+		System.out.println("1. Charger une classe");
+		System.out.println("2. Créer une classe");
+		System.out.println("3. Supprimer une classe");
+		System.out.println("4. Afficher les classes ");
+		System.out.println("5. Quitter");
+		System.out.print("→  Votre choix:");
+	}
 	public static void main(String[] arg) {
-		Etudiant etu1 = new Etudiant("azerty", "ytreza");
-		int a1 = 20, a2 = 345, a3 = 700;
-		int b1 = 56720, b2 = 9999, b3 = 20098;
-		int c1 = 233, c2 = 205, c3 = 1;
-		int d1 = 34, d2 = 0, d3 = 23;
-		etu1.ajouter_note(a1, a2, a3);
-		etu1.afficher();
-
+		
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+		int choix;
+		do {
+			mainMenu();
+			choix = in.nextInt();
+		}while (choix != 5);
+		
 	}
 }
